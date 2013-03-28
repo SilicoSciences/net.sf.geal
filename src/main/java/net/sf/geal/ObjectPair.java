9f532981137562c046e71dcf81103349948d2f49
Kerner <alexanderkerner24@gmail.com>
2013-03-28 14:41:45 +0100

    ObjectPair deprecated. Use net.sf.kerner.utils. instead.

--- src/main/java/net/sf/geal/ObjectPair.java
+++ src/main/java/net/sf/geal/ObjectPair.java
@@ -2,18 +2,36 @@ package net.sf.geal;
 
 import net.sf.kerner.utils.impl.util.Util;
 
+/**
+ * 
+ @deprecated use {@link net.sf.kerner.uitls.ObjectPair} instead
+ */
+@Deprecated
 public class ObjectPair<F, S> {
 
     private F first;
 
     private S second;
 
+    public ObjectPair() {
+    }
+
     public ObjectPair(final F first, final S second) {
         this.first = first;
         this.second = second;
     }
 
-    public ObjectPair() {
+    @Override
+    public boolean equals(final Object obj) {
+        return Util.equalsOnHashCode(this, obj);
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
     }
 
     @Override
@@ -25,29 +43,16 @@ public class ObjectPair<F, S> {
         return result;
     }
 
-    @Override
-    public boolean equals(final Object obj) {
-        return Util.equalsOnHashCode(this, obj);
+    public void setFirst(final F first) {
+        this.first = first;
+    }
+
+    public void setSecond(final S second) {
+        this.second = second;
     }
 
     @Override
     public String toString() {
         return getFirst() + "," + getSecond();
-    }
-
-    public F getFirst() {
-        return first;
-    }
-
-    public void setFirst(final F first) {
-        this.first = first;
-    }
-
-    public S getSecond() {
-        return second;
-    }
-
-    public void setSecond(final S second) {
-        this.second = second;
     }
 }
