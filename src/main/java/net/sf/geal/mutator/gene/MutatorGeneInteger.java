47c194db61c2bb64959bedc1788bbcbc4c8176a3
Kerner <alexanderkerner24@gmail.com>
2014-12-19 13:27:19 +0100

    Refactorings from jranges.

--- src/main/java/net/sf/geal/mutator/gene/MutatorGeneInteger.java
+++ src/main/java/net/sf/geal/mutator/gene/MutatorGeneInteger.java
@@ -1,12 +1,12 @@
 package net.sf.geal.mutator.gene;
 
-import net.sf.jranges.range.integer.IntegerRange;
-import net.sf.jranges.range.integer.impl.ZeroPositiveIntegerRange;
+import net.sf.jranges.range.integerrange.RangeInteger;
+import net.sf.jranges.range.integerrange.impl.ZeroPositiveIntegerRange;
 import net.sf.kerner.utils.math.UtilRandom;
 
 public class MutatorGeneInteger implements MutatorGene {
 
-    private final IntegerRange range;
+    private final RangeInteger range;
 
     public MutatorGeneInteger(final int low, final int high, final int interval) {
         range = new ZeroPositiveIntegerRange(low, high, interval);
